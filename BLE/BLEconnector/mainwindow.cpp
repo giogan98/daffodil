9ec5f0e92b2c62fb9fb85cce97aca30a92e1947b
Giorgio Gandolfi <giorgio.gandolfi98@gmail.com>
2021-11-24 17:17:38 +0100

    Updated BLEconnector to be able to scan for devices and connect to one

--- BLE/BLEconnector/mainwindow.cpp
+++ BLE/BLEconnector/mainwindow.cpp
@@ -6,10 +6,121 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowTitle("BLEconnector");
+    initializeBluetoothDeviceDiscoveryAgent();
 }
-
+//------------------------------------------------------------------------------
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+//------------------------------------------------------------------------------
+void MainWindow::initializeBluetoothDeviceDiscoveryAgent(void)
+{
+    ptr_deviceDiscoveryAgent = new QBluetoothDeviceDiscoveryAgent(this);
 
+    //Sets the maximum search time for BLE device search to timeout in ms:
+    ptr_deviceDiscoveryAgent->setLowEnergyDiscoveryTimeout(5000);
+
+    //This signal is emitted when an error occurs during Bluetooth device
+    //discovery. The error parameter describes the error that occurred.
+    connect(ptr_deviceDiscoveryAgent, &QBluetoothDeviceDiscoveryAgent::errorOccurred,
+            this, &MainWindow::scanError);
+
+    //'finished' signal is emitted when Bluetooth device discovery completes.
+    //The signal is not going to be emitted if the device discovery finishes
+    //with an error.
+    connect(ptr_deviceDiscoveryAgent, &QBluetoothDeviceDiscoveryAgent::finished,
+            this, &MainWindow::scanFinished);
+
+    //'canceled' signal is emitted when device discovery is aborted by a call to stop().
+    connect(ptr_deviceDiscoveryAgent, &QBluetoothDeviceDiscoveryAgent::canceled,
+            this, &MainWindow::scanFinished);
+}
+//------------------------------------------------------------------------------
+void MainWindow::scanError(QBluetoothDeviceDiscoveryAgent::Error error)
+{
+    if (error == QBluetoothDeviceDiscoveryAgent::PoweredOffError)
+    {
+        setError(tr("The Bluetooth adaptor is powered off."));
+    }
+    else if (error == QBluetoothDeviceDiscoveryAgent::InputOutputError)
+    {
+        setError(tr("Writing or reading from the device resulted in an error."));
+    }
+    else
+    {
+        setError(tr("An unknown error has occurred."));
+    }
+}
+//------------------------------------------------------------------------------
+void MainWindow::setError(const QString &error)
+{
+    if (m_error != error)
+    {
+        m_error = error;
+        emit errorChanged();
+    }
+}
+//------------------------------------------------------------------------------
+void MainWindow::setInfo(const QString &info)
+{
+    if (m_info != info)
+    {
+        m_info = info;
+        emit infoChanged();
+    }
+}
+//------------------------------------------------------------------------------
+void MainWindow::scanFinished(void)
+{
+    //Get the list of all the bluetooth discovered devices:
+    list_devicesInfos = ptr_deviceDiscoveryAgent->discoveredDevices();
+
+    //Cycle the list of all the bluetooth discovered devices and filter them
+    //based on the low energy configuration
+    //Display the LE devices in the combobox
+    for (const auto &element : list_devicesInfos)
+    {
+        if (element.coreConfigurations() & QBluetoothDeviceInfo::LowEnergyCoreConfiguration)
+        {
+            ui->cb_devices->addItem(element.name());
+        }
+    }
+    ui->cb_devices->addItem("A");
+}
+//------------------------------------------------------------------------------
+void MainWindow::on_pbn_search_clicked()
+{
+    //Starts Bluetooth device discovery, if it is not already started.
+    ptr_deviceDiscoveryAgent->start(QBluetoothDeviceDiscoveryAgent::LowEnergyMethod);
+}
+//------------------------------------------------------------------------------
+void MainWindow::on_pbn_okDevice_clicked()
+{
+    //Get index of the selected item in the combobox
+    int iIndex = ui->cb_devices->currentIndex();
+    setDevice(getDevice(iIndex));
+}
+//------------------------------------------------------------------------------
+QBluetoothDeviceInfo MainWindow::getDevice(const int &iIndex = 0)
+{
+    QBluetoothDeviceInfo bdiTemp;
+    if (list_devicesInfos.length() > iIndex)
+    {
+        bdiTemp = list_devicesInfos[iIndex];
+    }
+    else
+    {
+        qDebug()<<"Error: list_devicesInfos.lenght() is inferior to cb index";
+    }
+    qDebug()<<"Name: "<<bdiTemp.name()<<" Adress: "<<bdiTemp.address();
+    return bdiTemp;
+}
+//------------------------------------------------------------------------------
+void MainWindow::setDevice(const QBluetoothDeviceInfo &device)
+{
+    ptr_leController = QLowEnergyController::createCentral(device, this);
+    ptr_leController->connectToDevice();
+}
+//------------------------------------------------------------------------------
--- BLE/BLEconnector/mainwindow.h
+++ BLE/BLEconnector/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtBluetooth>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -12,10 +13,40 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
+    QBluetoothDeviceDiscoveryAgent *ptr_deviceDiscoveryAgent = nullptr;
+    QList<QObject*> m_devices;
+    QList<QBluetoothDeviceInfo> list_devicesInfos;
+    QString m_error;
+    QString m_info;
+    QLowEnergyController *ptr_leController = nullptr;
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void setError(const QString &error);
+    void setInfo(const QString &info);
+
+private:
+    void initializeBluetoothDeviceDiscoveryAgent(void);
+    QBluetoothDeviceInfo getDevice(const int &iIndex);
+    void setDevice(const QBluetoothDeviceInfo &device);
+
+public slots:
+    void scanError(QBluetoothDeviceDiscoveryAgent::Error error);
+    void scanFinished(void);
+
+signals:
+    void scanningChanged(); //Device Finder Class
+    void devicesChanged();  //Device Finder Class
+    void errorChanged(); //Bluetooth Base Class
+    void infoChanged();  //Bluetooth Base Class
+
+
+private slots:
+    void on_pbn_search_clicked();
+    void on_pbn_okDevice_clicked();
 };
 #endif // MAINWINDOW_H
